b2bc7e9bd6e2e5b70c8ae06320b9789b9ee74ccf
Enrique Marin <enrique_marin_h@hotmail.com>
2024-01-17 22:47:14 -0700

    feat: add App structure and SignIn Form

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <script setup lang="ts">
 // import HelloWorld from './components/HelloWorld.vue'
+import { useNotifications } from './composables/useNotifications';
+
+const { snack } = useNotifications();
 </script>
 
 <template>
-  <v-app>
+  <v-app class="app-container">
     <router-view></router-view>
+
+    <v-snackbar 
+      v-model="snack.show" 
+      :text="snack.message" 
+      :timeout="snack.duration" 
+      :color="snack.color"
+    >
+      <template v-slot:actions>
+        <v-btn
+          color="white"
+          variant="text"
+          @click="snack.show = false"
+        >
+          Cerrar
+        </v-btn>
+      </template>
+    </v-snackbar>
   </v-app>
 </template>
 
 <style scoped>
+.app-container {
+  height: 100dvh;
+  width: 100vw;
+  overflow: hidden;
+}
 .logo {
   height: 6em;
   padding: 1.5em;
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 // Vuetify
@@ -7,12 +8,15 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import '@/assets/styles.scss'
+
 const vuetify = createVuetify({
   components,
   directives,
 })
 
 const app = createApp(App)
+app.use(createPinia())
 app.use(router)
 app.use(vuetify)
 app.mount('#app')
